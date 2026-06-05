b7cff0008da26d92a15a8f1568a30f72cbcd9b21
Maksim Loknar <loknarmaksim@gmail.com>
2026-06-05 10:23:36 +0200

    Added setup functionblock example and built to run parquet writter

--- examples/applications/python/Function Blocks/parquet_writer.py
+++ examples/applications/python/Function Blocks/parquet_writer.py
@@ -1,60 +1,57 @@
-#!/usr/bin/env python
-
 ##
-# This script demonstrates how to use the ParquetRecorder function block
-# in Python to record data from a device's signal to a Parquet
-# file.
+# @tags: howto, function-blocks, recorder
+# @title: How to record data to Parquet
+##
+# Adds a ParquetRecorder function block, connects a signal,
+# records for a few seconds, and lists the output files.
 ##
 
-import opendaq as daq
-import time
 import os
+import time
+import opendaq as daq
+import sys
+sys.path.append("..")
+import Utils.daq_utils as daq_utils
 
-if __name__ == '__main__':
-    instance: daq.IInstance = daq.Instance()
-    # Here we use a reference device as a data source
-    device: daq.IDevice = instance.add_device(
-        instance.available_devices[0].connection_string)
-    print(f'Connected to device: {device.name}')
+if __name__ == "__main__":
+    try:
+        simulator = daq_utils.setup_simulator()
 
-    # We create a ParquetRecorder function block through the instance
-    # as a shortcut as the instance has module manager to load the module containing the function block
-    # and also has a scheduler that is neccessary for the function block to work
-    # The recorder will post writting tasks to the scheduler
-    # and will write the data to the Parquet file in the background
-    recoder_fb: daq.IFunctionBlock = instance.add_function_block(
-        'ParquetRecorder')
-    recoder: daq.IRecorder = daq.IRecorder.cast_from(recoder_fb)
-    print(f'Created function block: {recoder_fb.name}')
+        signals = simulator.get_signals_recursive()
+        if not signals:
+            print("No signals found", file=sys.stderr)
+            print("exit 1")
+            sys.exit(1)
 
-    # Here we connect an input port of the recorder to a signal of the device
-    # New input port is created every time we connect the signal to the recorder
-    input_port: daq.IInputPort = recoder_fb.input_ports[0]
-    signal: daq.ISignal = device.channels_recursive[0].signals[0]
-    input_port.connect(signal)
-    print(f'Connected input port: {input_port.name} to signal: {signal.name}')
+        recorder_fb = simulator.add_function_block("ParquetRecorder")
+        recorder = daq.IRecorder.cast_from(recorder_fb)
 
-    # The recorder has the only one property now - 'Path'
-    # which is the directory where the Parquet file will be saved
-    recoder_fb.set_property_value('Path', '.')
-    print(f'Set recording path to: {recoder_fb.get_property_value("Path")}')
+        recorder_fb.input_ports[0].connect(signals[0])
+        print(f"Connected {signals[0].name} to recorder")
 
-    # We call the start_recording method of the IRecorder interface that is implemented by the ParquetRecorder function block
-    recoder.start_recording()
-    print('Started recording...')
+        recorder_fb.set_property_value("Path", ".")
+        
+        recorder.start_recording()
+        print("Recording for 3 seconds...")
+        time.sleep(3)
+        recorder.stop_recording()
+        print("Stopped recording")
 
-    # wait for 5 sec
-    print('Recording for 5 seconds...')
-    time.sleep(5)
+        path = recorder_fb.get_property_value("Path")
+        parquet_files = [f for f in os.listdir(path) if f.endswith(".parquet")]
+        if parquet_files:
+            print(f"\nRecorded files:")
+            for f in parquet_files:
+                print(f"  {f}")
+        else:
+            print("No parquet files found", file=sys.stderr)
+            print("exit 1")
+            sys.exit(1)
 
-    recoder.stop_recording()
-    print('Stopped recording.')
+    except Exception as e:
+        print(f"Example failed: {e}", file=sys.stderr)
+        print("exit 1")
+        sys.exit(1)
 
-    # find and list all the parquet files in the recording path
-    parquet_files = [f for f in os.listdir(recoder_fb.get_property_value('Path')) if f.endswith('.parquet')]
-    if not parquet_files:
-        print('No parquet files found in the recording path.')
-    else:
-      print(f'Found parquet files:')
-      for file in parquet_files:
-          print(f' - {file}')
+    print("exit 0")
+    sys.exit(0)
