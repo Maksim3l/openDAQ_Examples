b8b59cb91ab951136e76872ff325e03bca7b5ecb
Maksim Loknar <loknarmaksim@gmail.com>
2026-06-05 12:51:26 +0200

    Scafolds for additional example ideas

--- examples/applications/python/create_instance.py
+++ examples/applications/python/create_instance.py
@@ -1,1 +1,14 @@
-# create Instance, print available device types
+##
+# @tags: 
+# @title: 
+##
+# Create an instance and print all avalible device types
+##
+
+import sys
+import opendaq as daq
+import Utils.daq_utils as daq_utils
+
+if __name__ == "__main__":
+    print("exit 0")
+    sys.exit(0)
